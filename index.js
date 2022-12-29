f34ec94322c23e0d4874aaa269a911a32b1543f6
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-29 20:50:58 +0100

    updated server

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
+app.use("/", (req, res) => {
+    res.send("Hello from server side.")
+});
+
 app.listen(PORT, () => {
     console.log(`server is running at PORT ${PORT}`);
 });
