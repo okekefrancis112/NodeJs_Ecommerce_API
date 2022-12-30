77a8f4a5b5080f2f9c0d0e7bba8fd714e42ccc52
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-30 03:17:44 +0100

    registration route updated

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const dotenv = require('dotenv').config();
 
 
 const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
 
 dbConnect();
 
 app.use("/", (req, res) => {
     res.send("Hello from server side.")
 });
+
+app.use('/api/user', authRouter);
 
 app.listen(PORT, () => {
     console.log(`server is running at PORT ${PORT}`);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,3 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/register');
+
+module.exports = router;
