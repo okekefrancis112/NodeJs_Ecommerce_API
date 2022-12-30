69836f643bab5a3b281a6b9d55551174d560f8b5
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-30 20:55:44 +0100

    create product implemented

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
 const cookieParser =  require("cookie-parser");
 
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/user', authRouter);
+app.use('/api/product', productRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ routes/productRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+     createProduct,
+     } = require("../controller/productCtrl");
+
+
+router.post('/', createProduct);
+
+module.exports = router;
